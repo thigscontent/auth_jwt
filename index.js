e457c6adcfe0a7abcde16aab38236acf4e09aeb2
thigscontent <contatothiagoawk@gmail.com>
2024-04-29 15:58:48 -0300

    Auth config

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const express = require("express"); // como se fosse importação de biblioteca
 const app = express(); // instanciando o app, uma propriedade que cria o servidor
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const port = 3000; //criando rota
+
+
+const port = process.env.PORT; //criando rota
 
 connectDB();
 
 app.use(express.json());
-app.use("/api/users", userRoutes);
+app.use("/users", userRoutes);
 
 
 // Retorno da porta que o servidor está rodando
@@ -19,101 +21,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
 
-// Home da API
-app.get("/", (req, res) => {
-  res.send("Pagina Inicial da API!");
-})
 
-// Rota Privada
-app.get('/user/:id', checkToken, async(req, res) => {
-  const user = await User.findById(id, '-password');
-  if(!user){
-    return res.status(404).json({msg: 'User not found'})
-  }
-  res.status(200).json(user);
-})
-//rota publica (MIddleware)
-function checkToken(req, res, next){
- const authHeader = req.headers['authorization'];
- const token = authHeader && authHeader.split(' ')[1];
- if(!token){
-   return res.status(401).json({msg: 'Acesso NEGADO'});
- }
-  try{
-    const secret = process.env.SECRET;
-    jwt.verify(token, secret);
-    next()
-  } catch(error){
-    res.status(400).json({msg: 'Token INVÁLIDO'});
-  }
-}
-// Pagina de registro dos usuários
-app.post("/auth/register", async (req, res) =>{
-  const {name, email, password} = req.body;
-  // Validações do usuário
-  if(!name) {
-    return res.status(400).json({error: "Não digitou nenhum nome!"});
-  }
-
-  if(!email) {
-    return res.status(400).json({error: "Não digitou nenhum email!"});
-  }
-
-  if(!senha) {
-    return res.status(400).json({error: "Não digitou nenhuma senha!"});
-  }
-  //verificar se usuário existe
-  const userExists = await User.findOne({email: email});
-  if(userExists){
-    return res.status(400).json({error: "O e-mail já existe!"});
-  }
-  //criptografar a senha
-  const salt = await bcrypt.genSalt(10);
-  const passHash = await bcrypt.hash(password, salt);
-  //criação de usuário
-  const user = new User({
-    name,
-    email,
-    password: passHash
-  })
-  try {
-    await user.save();
-    res.status(201).json("Usuário salvo! ");  
-  } catch(error) {
-    res.status(400).json({error: "Erro ao criar usuário!"});
-  }
-})
-
-//rota de login
-app.post("/auth/login", async (req, res) =>{
-  const {email, password} = req.body;
-  //validações
-  if(!email) {
-    return res.status(400).json({error: "Não digitou nenhum email!"});
-  }
-
-  if(!senha) {
-    return res.status(400).json({error: "Não digitou nenhuma senha!"});
-  }
-
-  //verificar se usuário existe
-  const user = await User.findOne({email: email});
-  if(!user){
-    return res.status(400).json({error: "usuario não encontrado"});
-  }
-
-  //verificar se a senha está correta
-  const checkPass = await bcrypt.compare(password, user.password);
-  if(!checkPass) {
-    return res.status(400).json({error: "Senha incorreta!"});
-  }
-
-  try{
-    const secret = process.env.SECRET
-    const token = jwt.sign({id: user._id}, secret,);
-    res.status(200).json({msg: 'Autenticação feita com sucesso!',token});
-  } catch (err) {
-    res.status(500).json({error: "Erro ao logar!"});
-  }
-})
-        
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -2,41 +2,21 @@ const User = require("../../src/models/User");
 
 class UserRepository {
   // crud é necessario implementar um método para cada ação do controller, assim como as regras do sql
-  async create() {
-    const user = new User(req.body);
-    try {
-      const savedUser = await user.save();
-      res.status(201).json(savedUser);
-    } catch (error) {
-      res.status(400).json({ message: error });
-    }
-  } // criar novo elemento
+  async create(userData){
+    const user = new User(userData);
+    await user.save();
+    return user;
+} // criar novo elemento
   async findAll() {
-    try {
-      res.status(200).json(users);
-      const users = await User.find();
-    } catch (error) {
-      res.status(400).json({ message: error });
-    }
+    return User.find();
   } // slecionar tudo, tipo select *
-  async update() {
-    try {
-      const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      });
-      res.status(200).json(updateUser);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+  
+  async updateById(id, userData) {
+      return User.findByIdAndUpdate(id, userData, { new: true })
   } // atuallizar
-  async delete() {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json({ message: "Usuario deletado com sucesso" });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  } // deletar
-}
 
+  async deleteById(id) {
+        return User.findByIdAndDelete(id)
+    }
+}
 module.exports = new UserRepository();
